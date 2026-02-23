72f85a4ad9175e03dfdbed660395c14237c23191
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2026-02-23 12:24:20 +0100

    add clean_df function

--- src/preprocess.py
+++ src/preprocess.py
@@ -18,3 +18,40 @@ def load_data(data_fraction=0.5):
 
 def save_dataset(df, path, version="v1"):
     df.to_csv(path+version+".csv", encoding='utf-8')
+
+
+def clean_dataset(payment_df):
+    # On remplace les NA par la moyenne
+    payment_df["prod_limit"] = payment_df["prod_limit"].fillna(
+        payment_df["prod_limit"].mean())
+    payment_df["highest_balance"] = payment_df["highest_balance"].fillna(
+        payment_df["highest_balance"].mean())
+
+    # Sépare les dates en 3 colonnes
+    payment_df["update_date"] = pd.to_datetime(payment_df["update_date"],
+                                               format="%d/%m/%Y")
+    payment_df["update_date_day"] = payment_df["update_date"].dt.day
+    payment_df["update_date_month"] = payment_df["update_date"].dt.month
+    payment_df["update_date_year"] = payment_df["update_date"].dt.year
+
+    payment_df["report_date"] = pd.to_datetime(payment_df["report_date"],
+                                               format="%d/%m/%Y")
+    payment_df["report_date_day"] = payment_df["report_date"].dt.day
+    payment_df["report_date_month"] = payment_df["report_date"].dt.month
+    payment_df["report_date_year"] = payment_df["report_date"].dt.year
+
+    payment_df = payment_df.drop(["report_date", "update_date"], axis=1)
+
+    payment_df["update_date_day"] = payment_df["update_date_day"].fillna(0)
+    payment_df["update_date_month"] = payment_df["update_date_month"].fillna(0)
+    payment_df["update_date_year"] = payment_df["update_date_year"].fillna(0)
+
+    payment_df["report_date_day"] = payment_df["report_date_day"].fillna(0)
+    payment_df["report_date_month"] = payment_df["report_date_month"].fillna(0)
+    payment_df["report_date_year"] = payment_df["report_date_year"].fillna(0)
+
+    cols = ["update_date_day", "update_date_month", "update_date_year",
+            "report_date_day", "report_date_month", "report_date_year"]
+
+    payment_df[cols] = payment_df[cols].astype(int)
+    return payment_df
