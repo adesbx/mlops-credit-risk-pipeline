53d6f0bdefabdbca43ec1673094ac150e4f29a0f
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2026-02-24 16:17:56 +0100

    More preprocess test

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -9,8 +9,11 @@ DATA_PATH_PAYMENT = BASE_DIR / "data" / "test" / "payment_test.csv"
 sys.path.append(os.path.abspath("./src"))
 
 import preprocess as utils_preprocess
+import numpy as np
 
-org_customer_df, org_payment_df = utils_preprocess.load_data(DATA_PATH_CUSTOMER, DATA_PATH_PAYMENT, 1.0)
+
+org_customer_df, org_payment_df = utils_preprocess.load_data(DATA_PATH_CUSTOMER, 
+                                                             DATA_PATH_PAYMENT, 1.0)
 payment_df = utils_preprocess.clean_dataset_payment(org_payment_df)
 customer_df = utils_preprocess.clean_dataset_customer(org_customer_df)
 
@@ -23,3 +26,21 @@ def test_na():
     global payment_df, customer_df
     assert payment_df.isna().sum().sum() == 0
     assert customer_df.isna().sum().sum() == 0
+
+def test_only_numeric_colums():
+    global payment_df, customer_df
+    no_df_num = payment_df.select_dtypes(exclude=[np.number])
+    assert no_df_num.shape[1] == 0
+    no_df_num = customer_df.select_dtypes(exclude=[np.number])
+    assert no_df_num.shape[1] == 0
+
+def test_numeric_colums():
+    global payment_df, customer_df
+    df_num = payment_df.select_dtypes(include=[np.number])
+    assert df_num.shape[1] > 0
+    df_num = customer_df.select_dtypes(include=[np.number])
+    assert df_num.shape[1] > 0
+
+def test_id_in_both():
+    global payment_df, customer_df
+    assert payment_df["id"].isin(customer_df["id"]).all()
