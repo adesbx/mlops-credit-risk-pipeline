d517fc46f337c667cdf5bee891a67ee63b84dbbe
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2026-02-27 12:23:28 +0100

    Add pipeline for train

--- src/train.py
+++ src/train.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
 
 
 def load_processed_data(path):
@@ -17,3 +21,27 @@ def split_data(df, target_col, test_size=0.3):
     )
 
     return X_train, X_test, y_train, y_test
+
+
+def build_pipeline(numerical_cols):
+    # On utilise ColumnTransformer si jamais on souhaite
+    # rajouter  des données catégorielles par la suite
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), numerical_cols),
+        ]
+    )
+
+    pipeline = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("model", LogisticRegression())
+        ]
+    )
+
+    return pipeline
+
+
+def train_model(pipeline, X_train, y_train):
+    pipeline.fit(X_train, y_train)
+    return pipeline
