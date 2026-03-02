1c4b6abbfd2c16b9f75d0cca368b46f61147693a
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2026-03-02 16:57:00 +0100

    Add workflow

---
+++ scripts/check_metrics.py
@@ -0,0 +1,26 @@
+import subprocess
+import json
+import sys
+
+def get_dvc_metrics_diff():
+    result = subprocess.run(
+        ["dvc", "metrics", "diff", "--json"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    
+    return json.loads(result.stdout)
+
+metrics_data = get_dvc_metrics_diff()
+accuracy_diff = metrics_data["models\\metrics.json"]["accuracy"]["diff"]
+recall_diff = metrics_data["models\\metrics.json"]["recall"]["diff"]
+
+if accuracy_diff < 0 or recall_diff < 0:
+    print("Accuracy or recall diff is < 0")
+    sys.exit(1)
+
+else:
+    print("Metrics ok")
+    sys.exit(0)
+
